ec42130db48870b0464b24af66b610a5497f4af8
meghana-meher <102056615+meghana-meher@users.noreply.github.com>
2023-10-12 16:17:25 +0530

    <HashRouter>

--- src/index.js
+++ src/index.js
@@ -12,11 +12,13 @@ import store from './redux/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
+  // <BrowserRouter>
+  <HashRouter>
   <Provider store={store}>
     <App />
     </Provider>
-  </BrowserRouter>
+  // </BrowserRouter>
+  </HashRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
