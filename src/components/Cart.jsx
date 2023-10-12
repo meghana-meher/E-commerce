7a71a8b1641dbe63b8848eaa2913ef6431bc729a
maggie <meghanameher.tenneti@gmail.com>
2023-10-12 14:28:49 +0530

    my first react commit

---
+++ src/components/Cart.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { useSelector } from 'react-redux'
+
+const Cart = () => {
+  return (
+    <div >
+      <h1 className="text-center my-5">HAPPY SHOPPING!!!</h1>
+      <p className='text-center'>Fashion is a form of self-expression and autonomy at a particular period and place and in a specific context, of clothing, footwear, lifestyle, accessories, makeup, hairstyle, and body posture.[1] The term implies a look defined by the fashion industry as that which is trending. Everything that is considered fashion is available and popularized by the fashion system (industry and media).</p>
+    <p className='text-center'>The French word mode, meaning "fashion", dates as far back as 1482, while the English word denoting something "in style" dates only to the 16th century. Other words exist related to concepts of style and appeal that precede mode. In the 12th and 13th century Old French the concept of elegance begins to appear in the context of aristocratic preferences to enhance beauty and display refinement, and cointerie, the idea of making oneself more attractive to others by style or artifice in grooming and dress, appears in a 13th-century poem by Guillaume de Lorris advising men that "handsome clothes and handsome accessories improve a man a great deal".[4]
+
+Fashion scholar Susan B. Kaiser states that everyone is "forced to appear", unmediated before others.[5] Everyone is evaluated by their attire, and evaluation includes the consideration of colors, materials, silhouette, and how garments appear on the body. Garments identical in style and material also appear different depending on the wearer's body shape, or whether the garment has been washed, folded, mended, or is new.
+
+Fashion is defined in a number of different ways, and its application can be sometimes unclear. Though the term fashion connotes difference, as in "the new fashions of the season", it can also connote sameness, for example in reference to "the fashions of the 1960s", implying a general uniformity. Fashion can signify the latest trends, but may often reference fashions of a previous era, leading to the reappearance of fashions from a different time period. While what is fashionable can be defined by a relatively insular, esteemed and often rich aesthetic elite who make a look exclusive, such as fashion houses and haute couturiers, this 'look' is often designed by pulling references from subcultures and social groups who are not considered elite, and are thus excluded from making the distinction of what is fashion themselves.
+
+Whereas a trend often connotes a peculiar aesthetic expression, often lasting shorter than a season and being identifiable by visual extremes, fashion is a distinctive and industry-supported expression traditionally tied to the fashion season and collections.[6] Style is an expression that lasts over many seasons and is often connected to cultural movements and social markers, symbols, class, and culture (such as Baroque and Rococo). According to sociologist Pierre Bourdieu, fashion connotes "the latest difference."[7]
+
+Even though the terms fashion, clothing and costume are often used together, fashion differs from both. Clothing describes the material and the technical garment, devoid of any social meaning or connections; costume has come to mean fancy dress or masquerade wear. Fashion, by contrast, describes the social and temporal system that influences and "activates" dress as a social signifier in a certain time and context. Philosopher Giorgio Agamben connects fashion to the qualitative Ancient Greek concept of kairos, meaning "the right, critical, or opportune moment", and clothing to the quantitative concept of chronos, the personification of chronological or sequential time.[8]
+
+While some exclusive brands may claim the label haute couture, the term is technically limited to members of the Chambre Syndicale de la Haute Couture[9] in Paris.[6] Haute couture is more aspirational; inspired by art and culture, and in most cases, reserved for the economic elite.
+
+Fashion is also a source of art, allowing people to display their unique tastes and styling.[10] Different fashion designers are influenced by outside stimuli and reflect this inspiration in their work. For example, Gucci's 'stained green' jeans[11] may look like a grass stain, but to others, they display purity, freshness, and summer.[1]
+
+Fashion is unique, self-fulfilling and may be a key part of someone's identity. Similarly to art, the aims of a person's choices in fashion are not necessarily to be liked by everyone, but instead to be an expression of personal taste.[10] A person's personal style functions as a "societal formation always combining two opposite principles. It is a socially acceptable and secure way to distinguish oneself from others and, at the same time, it satisfies the individual's need for social adaptation and imitation."[12] While philosopher Immanuel Kant believed that fashion "has nothing to do with genuine judgements of taste", and was instead "a case of unreflected and 'blind' imitation",[12] sociologist Georg Simmel[13] thought of fashion as something that "helped overcome the distance between an individual and his society".[12]</p>
+    </div>
+  )
+}
+
+export default Cart;
