311ce3916f785984acc931950e6bc89f2e2cae7f
meghana-meher <102056615+meghana-meher@users.noreply.github.com>
2023-10-12 15:15:15 +0530

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,7 +5,7 @@ const Home = () => {
     return (
         <div classNameName='hero'>
             <div className="card text-bg-dark text-white border-0">
-                <img src="assests/bg.jpg" className="card-img" alt="Background" height="700px"/>
+                <img src="/assests/bg.jpg" className="card-img" alt="Background" height="700px"/>
                 <div className="card-img-overlay d-flex flex-column justify-content-center">
                     <div className="container">
                     <h5 className="card-title display-3 fw-bolder mb-0">NEW SEASON ARRIVALS</h5>
